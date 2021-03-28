2818ca5e1da14abe2fddc161574027a996132da0
pixar2390 <pixar_duffy@icloud.com>
2021-03-29 00:53:06 +0900

    210329

--- Chapter02/all/js/main.js
+++ Chapter02/all/js/main.js
@@ -2,6 +2,14 @@ $(function(){
 	$('#typo').css('color','#ebc000');
 });
 
+$(function(){
+	$('#typo .inner').css('transform', 'rotate(10deg)');
+});
+
+$(function name(params) {
+	$('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
+});
+
 // テスト
 // marge_test
 // githubテスト
