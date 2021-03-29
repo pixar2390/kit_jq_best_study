48ad588d26844f50465db3c963f2f9ead7153696
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-03-29 13:02:30 +0900

    210329

--- Chapter02/all/js/main.js
+++ Chapter02/all/js/main.js
@@ -1,13 +1,12 @@
 $(function(){
 	$('#typo').css('color','#ebc000');
-});
-
-$(function(){
-	$('#typo .inner').css('transform', 'rotate(10deg)');
-});
-
-$(function name(params) {
+	$('#typo .inner').css('transform', 'rotate(20deg)');
 	$('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
+	$('#typo').css({
+		fontSize: '50px',
+		backgroundColor: '#ae5e9b',
+		color: '#ebc012'
+	});
 });
 
 // テスト
