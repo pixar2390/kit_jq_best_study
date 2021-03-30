581e1dfa4f5e66e868ff30165065d1211d06590e
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-03-30 13:44:27 +0900

    210330

--- Chapter02/all/js/main.js
+++ Chapter02/all/js/main.js
@@ -1,14 +1,26 @@
 $(function(){
-	$('#typo').css('color','#ebc000');
+	$('#typo')
+		.on('mouseover', function(){
+			$('#typo').css('color','#ebc000');
+		})
+		.on('mouseout', function(){
+			$('#typo').css('color','#FFFFF');
+		});
+
+
+	$('.page-main .inner').on('click', function(){
+		$('.page-main .inner').css('transform','rotate(0deg)');
+		$('.page-footer').css('background-color','#CEE777')
+	});
+
 	$('#typo .inner').css('transform', 'rotate(20deg)');
 	$('.page-main > div:nth-child(1) .inner').css('opacity', 0.5);
 	$('#typo').css({
-		fontSize: '50px',
+		fontSize: '90px',
 		backgroundColor: '#ae5e9b',
-		color: '#ebc012'
+		// color: '#ebc012'
 	});
 });
-
 // テスト
 // marge_test
 // githubテスト
