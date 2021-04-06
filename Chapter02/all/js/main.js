46be656ff3696a2d99b04908db98f403be57f94f
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-04-06 13:15:30 +0900

    210406

--- Chapter02/all/js/main.js
+++ Chapter02/all/js/main.js
@@ -46,10 +46,50 @@ var added3 = added1 + added2;
 function add(num1, num2) {
 	return num1 + num2;
 }
-console.log(added1);
+// console.log(added1);
 
 var num = 10 - 5 * 345/97;
-console.log(num);
+// console.log(num);
+
+// var a=100;
+// var b=200;
+// var c;
+
+// if (a==200) {
+// 	c=a;
+// }else if(b==200){
+// 	c=b;
+// }else{
+// 	c=300;
+// }
+
+// switch (500) {
+// 	case a:
+// 		c=a;
+// 		break;
+// 	case b:
+// 		c=b;
+// 		break;
+// 	default:
+// 		c=300;
+// 		break;
+// }
+
+
+var a = [1,2,3,4,5,6];
+var b = 0;
+for (var i = 0, len = a.length; i < len; i++) {
+	b += a[i];
+	console.log(b);
+}
+
+console.log(b);
+console.log(a.length);
+console.log(len);
+
+
+
+
 // テスト
 // marge_test
 // githubテスト
