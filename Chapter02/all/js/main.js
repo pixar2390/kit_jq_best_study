a31d7b016c49d166820c051295dcd5d23cdd2016
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-04-07 12:46:00 +0900

    210407_1

--- Chapter02/all/js/main.js
+++ Chapter02/all/js/main.js
@@ -79,13 +79,17 @@ var num = 10 - 5 * 345/97;
 var a = [1,2,3,4,5,6];
 var b = 0;
 for (var i = 0, len = a.length; i < len; i++) {
+	var num = a[i];
+	if (num % 5 === 0) {
+		break;
+	}else if (num % 2 === 1) {
+		continue;
+	}
 	b += a[i];
-	console.log(b);
 }
 
 console.log(b);
-console.log(a.length);
-console.log(len);
+
 
 
 
