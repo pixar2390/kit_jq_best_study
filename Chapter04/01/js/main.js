0e68e9a8fe4149e3934415d0f74aec1c80b92fdf
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-04-12 13:02:20 +0900

    210412_kit

--- Chapter04/01/js/main.js
+++ Chapter04/01/js/main.js
@@ -1,5 +1,20 @@
 $(function () {
-    $('#buttons1 button:nth-child(4)').css('background-color', '#ddd');
+    var duration = 300;
+    // buttons1---------------------------------------------------------
+    // buttons1 1行目
+    $('#buttons1 button:nth-child(-n+4)')
+        .on('mouseover', function(){
+            $(this).stop(true).animate({
+                backgroundColor: '#ae5e9b',
+                color: '#fff'
+            },duration);
+        })
+        .on('mouseout', function(){
+            $(this).stop(true).animate({
+                backgroundColor: '#fff',
+                color: '#ebc000'
+            },duration);
+        });
 });
 
 
