3552300eb3e9f162902e9b083e1e8915499553c7
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-04-22 13:01:53 +0900

    210422

--- Chapter04/02/js/main.js
+++ Chapter04/02/js/main.js
@@ -18,16 +18,24 @@ $(function(){
     // images 2つ目の画像
     $images.filter(':nth-child(2)')
         .on('mouseover', function () {
-            $(this).find('span').stop(true).animate({
+            $(this).find('strong').stop(true).animate({
+                left:'0%',
                 opacity:1
             },duration);
-            $(this).find('strong').stop(true).animate({
-                left: '0%',
+            $(this).find('span').stop(true).animate({
                 opacity:1
             },duration);
         })
         .on('mouseout', function () {
-
+            $(this).find('strong').stop(true).animate({
+                left: '-200%',
+                opacity:0
+            },duration);
+            $(this).find('span').stop(true).animate({
+                opacity:0
+            },duration);
         });
+    // images 3つ目の画像
+
 
 });
