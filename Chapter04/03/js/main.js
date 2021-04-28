8b71d72e07747261beb3e93e411a5b455dfb3910
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-04-28 13:24:33 +0900

    子要素フィルターのコロンの前に要素名を指定しても意味がないよ

--- Chapter04/03/js/main.js
+++ Chapter04/03/js/main.js
@@ -1,5 +1,9 @@
 $(function () {
 	var duration = 500;
+	// $('.test').find('p:nth-child(2)').css('color','#FF851B');
+	// $('.test').find('p:last-child').css('color','#FF851B');
+	$('.test :nth-child(2)').css('color','#FF851B');
+	$('.test :last-child').css('color','#FF851B');
 	// button2--------------------------------
 	$('#buttons2 button').each(function(index){
 		var pos = index * 40-40;
@@ -10,10 +14,18 @@ $(function () {
 			backgroundColor: '#faee00',
 			color: '#000'
 		}, duration);
-		$btn.find('img:first-child').stop(true).animate({
+
+		// セレクタの検証
+		// console.log($(this).prop("tagName"));
+		// console.log($(this).find('img:first-child').prop("tagName"));
+		// console.log($(this).find('img:last-child').prop("tagName"));
+
+		// filterの":first-child"はコロンの前に要素名を入れても意味がない。全ての子要素の中から最初の要素が選ばれる
+		$btn.find(':first-child').stop(true).animate({
 			opacity: 0
 		},duration);
-		$btn.find('img:last-child').stop(true).animate({
+		// filterの":last-child"はコロンの前に要素名を入れても意味がない。全ての子要素の中から最後の要素が選ばれる
+		$btn.find(':last-child').stop(true).animate({
 			opacity: 1
 		},duration);
 	})
