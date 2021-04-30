382ad93822a370b1616d222e1c17eeb19640cf69
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-04-30 18:11:03 +0900

    210430

--- Chapter04/03/js/main.js
+++ Chapter04/03/js/main.js
@@ -2,8 +2,8 @@ $(function () {
 	var duration = 500;
 	// $('.test').find('p:nth-child(2)').css('color','#FF851B');
 	// $('.test').find('p:last-child').css('color','#FF851B');
-	$('.test :nth-child(2)').css('color','#FF851B');
-	$('.test :last-child').css('color','#FF851B');
+	// $('.test :nth-child(2)').css('color','#FF851B');
+	// $('.test :last-child').css('color','#FF851B');
 	// button2--------------------------------
 	$('#buttons2 button').each(function(index){
 		var pos = index * 40-40;
@@ -21,23 +21,23 @@ $(function () {
 		// console.log($(this).find('img:last-child').prop("tagName"));
 
 		// filterの":first-child"はコロンの前に要素名を入れても意味がない。全ての子要素の中から最初の要素が選ばれる
-		$btn.find(':first-child').stop(true).animate({
+		$btn.find('img:first-child').stop(true).animate({
 			opacity: 0
 		},duration);
 		// filterの":last-child"はコロンの前に要素名を入れても意味がない。全ての子要素の中から最後の要素が選ばれる
-		$btn.find(':last-child').stop(true).animate({
+		$btn.find('img:nth-child(2)').stop(true).animate({
 			opacity: 1
 		},duration);
 	})
 	.on('mouseout', function () {
 		var $btn = $(this).stop(true).animate({
 			backgroundColor: '#FFFFFF',
-			color: '#29333A'
+			color: '#01b169'
 		},duration)
 		$btn.find('img:first-child').stop(true).animate({
 			opacity: 1
 		},duration);
-		$btn.find('img:last-child').stop(true).animate({
+		$btn.find('img:nth-child(2)').stop(true).animate({
 			opacity:0
 		});
 	});
