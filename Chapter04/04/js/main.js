8ea4a8ba5d1f86af8993472e2f16b55afc48b001
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-05-12 13:08:26 +0900

    210512

--- Chapter04/04/js/main.js
+++ Chapter04/04/js/main.js
@@ -1,19 +1,12 @@
-$(function(){
-    // 
-    var duration = 300;
+$(function() {
+	var duration = 300;
 
-    // aside ----------------------------------------
-    var $aside = $('.page-main > aside');
-    var $asidButton = $aside.find('button')
-        .on('click', function(){
-            $aside.toggleClass('open');
-            if($aside.hasClass('open')){
-                $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
-                $asidButton.find('img').attr('src', 'img/btn_close.png');
-            }else{
-                $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
-                $asidButton.find('img').attr('src', 'img/btn_open.png');
-            };
-        });
+	//aside--------------------------------------------------------
+	var $aside = $('.page-main > aside');
+	var $asideButton = $aside.find('button');
+	$asideButton.on('click', function(){
+		$aside.css('left', '0px')
+	});
+
 
 });
