3b1d470cf22b1e105e99ac8b09e777f106f5f014
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-06-04 19:58:38 +0900

    210604_1

---
+++ Chapter05/03/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+	/*
+	 *Sticky header
+	 */
+	$('.page-header').each(function(){
+		var $window = $(window), //ウィンドウをjQueryオブジェクト化
+			 $header = $(this),   //ヘッダーをjQueryオブジェクト化
+			 //ヘッダーのデフォルトの位置を変数に格納
+			 headerOffsetTop = $header.offset().top;
+
+		//ウィンドウのスクロールイベントを監視
+	});
+});
