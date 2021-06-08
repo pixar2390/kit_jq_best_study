a596cf71a7a9f55393d8286e1fbc2154bd8783a1
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-06-08 13:01:43 +0900

    210608

--- Chapter05/03/js/main.js
+++ Chapter05/03/js/main.js
@@ -10,5 +10,21 @@ $(function () {
 			 headerOffsetTop = $header.offset().top;
 
 		//ウィンドウのスクロールイベントを監視
+		//(ウィンドウがスクロールするごとに処理を実行)
+		$window.on('scroll', function(){
+			//ウィンドウのスクロール量をチェックし
+			//ヘッダーのデフォルト位置を過ぎていればクラスを付与
+			//そうでなければ削除
+			if ($window.scrollTop() > headerOffsetTop) {
+				$header.addClass('sticky');
+			} else {
+				$header.removeClass('sticky');
+			}
+		});
+
+		//ウィンドウのスクロールイベントを発生させる
+		//（ヘッダーの初期位置を調整するため）
+		$window.trigger('scroll');
+
 	});
 });
