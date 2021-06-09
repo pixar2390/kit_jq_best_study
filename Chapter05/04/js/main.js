6e81284f4358dbe6861e800309aa30e34bfa3365
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-06-09 13:04:27 +0900

    210609

--- Chapter05/04/js/main.js
+++ Chapter05/04/js/main.js
@@ -1,39 +1,25 @@
-$(function () {
+$(function(){
 
     /*
-     * Sticky header
+     * sticky header
      */
-    $('.page-header').each(function () {
-
-        var $window = $(window), // Window オブジェクト
-            $header = $(this),   // ヘッダー
+    $('.page-header').each(function() {
+        var $window = $(window), //Windowオブジェクト
+            $header = $(this),  //ヘッダー
 
             // ヘッダーのクローン
             $headerClone = $header.contents().clone(),
 
-            // ヘッダーのクローンのコンテナー
+            //ヘッダーのクローンのコンテナー
             $headerCloneContainer = $('<div class="page-header-clone"></div>'),
 
-            // HTML の上辺からヘッダーの底辺までの距離 = ヘッダーのトップ位置 + ヘッダーの高さ
+            // HTMLの上辺からヘッダーの底辺までの距離 = ヘッダーのトップ位置 + ヘッダーの高さ
+            //threshold(ˈTHreSHˌ(h)ōld_閾値)
             threshold = $header.offset().top + $header.outerHeight();
 
-        // コンテナーにヘッダーのクローンを挿入
-        $headerCloneContainer.append($headerClone);
+        //コンテナーにヘッダーのクローンを挿入
 
-        // コンテナーを body の最後に挿入
-        $headerCloneContainer.appendTo('body');
 
-        // スクロール時に処理を実行するが、回数を 1 秒間あたり 15 までに制限
-        $window.on('scroll', $.throttle(1000 / 15, function () {
-            if ($window.scrollTop() > threshold) {
-                $headerCloneContainer.addClass('visible');
-            } else {
-                $headerCloneContainer.removeClass('visible');
-            }
-        }));
-
-        // スクロールイベントを発生させ、初期位置を決定
-        $window.trigger('scroll');
     });
 
 });
