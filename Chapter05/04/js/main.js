baf66dcaaa63b7f0855c9c2bb0ca819d662cd071
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-06-10 13:02:07 +0900

    210610

--- Chapter05/04/js/main.js
+++ Chapter05/04/js/main.js
@@ -18,8 +18,22 @@ $(function(){
             threshold = $header.offset().top + $header.outerHeight();
 
         //コンテナーにヘッダーのクローンを挿入
+        $headerCloneContainer.append($headerClone);
 
+        //コンテナーをbodyの最後に挿入
+        $headerCloneContainer.appendTo('body');
 
+        //スクロール時に処理を実行するが、回数を1秒間あたり15までに制限
+        $window.on('scroll', $.throttle(1000 / 15, function() {
+            if ($window.scrollTop() > threshold) {
+                $headerCloneContainer.addClass('visible');
+            } else {
+                $headerCloneContainer.removeClass('visible');
+            }
+        }));
+
+        //スクロールイベントを発生させ、初期位置を決定
+        $window.trigger('scroll');
     });
 
 });
