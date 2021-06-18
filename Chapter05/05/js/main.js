d00b90bba87af00791507afaf1dcf6c0a6fe1963
yasutaka_kit <nishijima.yasutaka@k-idea.jp>
2021-06-18 16:59:25 +0900

    210618

---
+++ Chapter05/05/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	/*
+	 * Tabs
+	 */
+	$('#works').each(function(){
+
+		//タブの各要素をjQueryオブジェクト化
+		var $tabList   =$(this).find('.tabs-nav'),  //タブのリスト
+			$tabAnchors = $tabList.find('a'),
+
+	});
+
+});
