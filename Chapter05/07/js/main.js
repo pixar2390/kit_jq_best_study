9b78e2c38d62104620775b3b7d5714343cd3bdce
pixar2390 <pixar_duffy@icloud.com>
2021-07-05 21:02:42 +0900

    210705

--- Chapter05/07/js/main.js
+++ Chapter05/07/js/main.js
@@ -16,7 +16,21 @@ $(function(){
     //scrollTopが利用できる要素を検出する関数
     function scrollableElement(){
         var i, len, el, $el, scrollable;
-
+        for(i = 0, len = arguments.length; i < len; i++) {
+            el = arguments[i],
+            $el = $(el);
+            if ($el.scrollTop() > 0) {
+                return el;
+            } else {
+                $el.scrollTop(1);
+                scrollable = $el.scrollTop() > 0;
+                $el.scrollTop(0);
+                if (scrollable) {
+                    return el;
+                }
+            }
+        }
+        return[];
     }
 
 
